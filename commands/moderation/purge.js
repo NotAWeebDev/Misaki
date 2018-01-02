01c0f37cabfbaa928ca5653c67959fd54675432a
York <aarghgames@gmail.com>
2018-01-02 20:56:09 +0000

    awaiting

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -17,9 +17,9 @@ class Purge extends Command {
     if (args[0].isNumber() == true && args[1] == null) {
       if (args[0] < 2 || args[0] > 100) return message.channel.send("Unable to purge messages, invalid integer for purge amount. Value must be between 2 and 100 messages.").then((msg) => msg.delete(5000));
       const todelete = await message.channel.messages.fetch({limit: args[0]});
-      message.channel.bulkDelete(todelete).catch(error => console.log(error.stack));
+      await message.channel.bulkDelete(todelete).catch(error => console.log(error.stack));
       const reply = await this.client.responses.purgeMessages.random().replaceAll("{{amount}}", todelete.size);
-      message.channel.send(reply).then((msg) => msg.delete(5000));
+      await message.channel.send(reply).then((msg) => msg.delete(5000));
     }
   }
 }
