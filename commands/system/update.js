2a81e6d93cdfbcdbda198b39995f5f7c3ec1607f
York <aarghgames@gmail.com>
2018-01-02 13:50:29 +0000

    Added update command

---
+++ commands/system/update.js
@@ -0,0 +1,34 @@
+const Command = require(`${process.cwd()}/base/Command.js`);
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
+const path = require("path");
+
+class Update extends Command {
+  constructor(client) {
+    super(client, {
+      name: "update",
+      description: "This updates the bot from its git repo.",
+      usage: "update",
+      category: "System",
+      extended: "This command is designed to update the bot from it's own repository, then reboots the bot for the changes to take effect.",
+      hidden: true,
+      aliases: ["git", "pull"],
+      botPerms: [],
+      permLevel: "Bot Owner"
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    const { stdout, stderr, err } = await exec(`git pull ${require(`${process.cwd()}/package.json`).repository.url.split("+")[1]}`, { cwd: path.join(__dirname, "../../") }).catch(err => ({ err }));
+    if (err) return console.error(err);
+    const out = [];
+    if (stdout) out.push(stdout);
+    if (stderr) out.push(stderr);
+    await message.channel.send(out.join("---\n"), { code: true });
+    if (!stdout.toString().includes("Already up-to-date.")) {
+      this.client.commands.get("reboot").run(message, args, level);
+    }
+  }
+}
+
+module.exports = Update;
