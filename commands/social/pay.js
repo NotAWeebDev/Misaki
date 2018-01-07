90e92b429370bd5e6279845974e2d123aa673116
York <aarghgames@gmail.com>
2018-01-07 22:00:21 +0000

    Bugfix
    Fixed corruption of points if the number is to high.

--- commands/social/pay.js
+++ commands/social/pay.js
@@ -19,6 +19,9 @@ class Pay extends Social {
       const [bot, user] = await this.verifySocialUser(message, args[0]);
       if (bot) return message.response("❗", "Bot's cannot accumulate points or levels.");
       if (isNaN(args[1])) return message.response(undefined, "Not a valid amount");
+
+      if (parseInt(args[1]) > Number.MAX_SAFE_INTEGER) return message.response(undefined, "That number is to high");
+      
       if (args[1] < 0) return message.response(undefined, "You cannot pay less than zero, whatcha trying to do? rob em?");
       else if (args[1] < 1) return message.response(undefined, "You paying 'em with air? boi don't make me slap you 👋");
       if (message.author.id === user) return message.response(undefined, "You cannot pay yourself, why did you even try it?");
