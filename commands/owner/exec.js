52dc57a59518dd57e1fb2d560a2fbe5361119a32
York <aarghgames@gmail.com>
2018-01-09 22:31:01 +0000

    Codacy styling

--- commands/owner/exec.js
+++ commands/owner/exec.js
@@ -1,22 +1,22 @@
 const Command = require(`${process.cwd()}/base/Command.js`);
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 
 class Exec extends Command {
   constructor(client) {
     super(client, {
-      name: 'exec',
-      description: 'Executes a new process, very dangerous.',
-      usage: 'exec <expression>',
-      aliases: ['shell'],
-      category: 'System',
-      extended: 'This will spawn a child process and execute the given command.',
-      botPerms: ['SEND_MESSAGES'],
-      permLevel: 'Bot Owner'
+      name: "exec",
+      description: "Executes a new process, very dangerous.",
+      usage: "exec <expression>",
+      aliases: ["shell"],
+      category: "System",
+      extended: "This will spawn a child process and execute the given command.",
+      botPerms: ["SEND_MESSAGES"],
+      permLevel: "Bot Owner"
     });
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
-    exec(`${args.join(' ')}`, (error, stdout) => {
+    exec(`${args.join(" ")}`, (error, stdout) => {
       const response = (error || stdout);
       message.channel.send(`Ran: ${message.content}\n\`\`\`${response}\`\`\``, {split: true}).catch(console.error);
     });
