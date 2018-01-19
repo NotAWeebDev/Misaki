9fffbbbd57a25dba8a934c8d7c64af37473023df
York <aarghgames@gmail.com>
2018-01-19 23:43:41 +0000

    DBL Stats.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,9 +1,16 @@
+const snekfetch = require("snekfetch");
 module.exports = class {
   constructor(client) {
     this.client = client;
   }
 
   async run(guild) {
+
+    snekfetch.post(`https://discordbots.org/api/bots/${this.client.user.id}/stats`)
+      .set("Authorization", this.client.config.dblToken)
+      .send({ server_count: this.client.guilds.size })
+      .then(console.log("Sent guild count to discordbots.org!"));
+
     this.client.user.setActivity(`@${this.client.user.username} help | ${this.client.guilds.size} Servers`);
     this.client.logger.log(`New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount - 1} members`);
   }
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,9 +1,17 @@
+const snekfetch = require("snekfetch");
 module.exports = class {
   constructor(client) {
     this.client = client;
   }
 
   async run(guild) {
+
+    snekfetch.post(`https://discordbots.org/api/bots/${this.client.user.id}/stats`)
+      .set("Authorization", this.client.config.dblToken)
+      .send({ server_count: this.client.guilds.size })
+      .then(console.log("Sent guild count to discordbots.org!"));
+
+
     this.client.user.setActivity(`@${this.client.user.username} help | ${this.client.guilds.size} Server${this.client.guilds.size > 1 ? "s" : ""}`);
     this.client.settings.delete(guild.id);
     this.client.reminders.findAll("guildid", guild.id).forEach((i) => {
