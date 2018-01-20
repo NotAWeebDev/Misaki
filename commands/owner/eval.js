41448e0248a74e19c9e2cfeabcc0f92d3696855b
Yukine <blazylp@gmail.com>
2018-01-20 17:52:29 +0100

    make eval great again

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,4 +1,6 @@
 const Owner = require(`${process.cwd()}/base/Owner.js`);
+const { inspect } = require("util");
+const { post } = require("snekfetch");
 
 class Eval extends Owner {
   constructor(client) {
@@ -13,15 +15,37 @@ class Eval extends Owner {
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
+    const { client } = message;
+    const { clean } = this;
     const code = args.join(" ");
+    const token = client.token.split("").join("[^]{0,2}");
+    const rev = client.token.split("").reverse().join("[^]{0,2}");
+    const filter = new RegExp(`${token}|${rev}`, "g");
     try {
-      const asyncCode = `(async() => ${code} )()`;
-      const evaled = await eval(asyncCode);
-      const clean = await this.client.clean(this.client, evaled);
-      message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${await this.client.clean(this.client, err)}\n\`\`\``);
+      let output = eval(code);
+      if (output instanceof Promise) output = await output;
+      output = inspect(output, { depth: 0, maxArrayLength: null });
+      output = output.replace(filter, "[TOKEN]");
+      output = clean(output);
+      if (output.length < 1950) {
+        message.channel.send(`\`\`\`js\n${output}\n\`\`\``);
+      } else {
+        try {
+          const { body } = await post("https://www.hastebin.com/documents").send(output);
+          message.channel.send(`Output was to long so it was uploaded to hastebin https://www.hastebin.com/${body.key}.js `);
+        } catch (error) {
+          message.channel.send(`I tried to upload the output to hastebin but encountered this error ${error.name}:${error.message}`);
+        }
+      }
+    } catch (error) {
+      message.channel.send(`The following error occourd \`\`\`js\n${error.stack}\`\`\``);
     }
+  }
+
+  clean(text)  {
+    return text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203));
   }
 }
 
--- modules/functions.js
+++ modules/functions.js
@@ -30,20 +30,6 @@ module.exports = (client) => {
     }
   };
 
-  client.clean = async (client, text) => {
-    if (text && text.constructor.name == "Promise")
-      text = await text;
-    if (typeof evaled !== "string")
-      text = require("util").inspect(text, {depth: 0});
-
-    text = text
-      .replace(/`/g, "`" + String.fromCharCode(8203))
-      .replace(/@/g, "@" + String.fromCharCode(8203))
-      .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
-
-    return text;
-  };
-
   client.randomNum = async (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
