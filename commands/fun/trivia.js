382f7adcf40ed6a41f289239a302807003a379fe
AndrewJWin <fastroute66@live.com>
2018-01-24 08:55:22 -0600

    Amended from Kyra's suggestions

--- commands/fun/trivia.js
+++ commands/fun/trivia.js
@@ -1,7 +1,9 @@
 const Social = require(`${process.cwd()}/base/Social.js`);
 const Discord = require("discord.js");
+const html = require("html-entities").AllHtmlEntities;
 const snek = require("snekfetch");
-const B64 = require("base-64");
+const h = new html();
+
 class Trivia extends Social {
   constructor(client) {
     super(client, {
@@ -15,48 +17,52 @@ class Trivia extends Social {
       botPerms: ["EMBED_LINKS"]
     });
   }
+
   async run(message, args, level) { // eslint-disable-line no-unused-vars
-
-    const inf = await snek.get("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple&encode=base64");
-    const ob = await inf.body;
-    const emb = new Discord.MessageEmbed();
-    var a;
-    var b;
-    var c;
-    var d;
-    var choice;
-    const questions = [];
-
-    ob.results[0].incorrect_answers.forEach(ob => questions.push(B64.decode(ob)));
-    questions.push(B64.decode(ob.results[0].correct_answer));
+    const { body } = await snek.get("https://opentdb.com/api.php?amount=50&difficulty=medium&type=multiple"); // Grab the questions.
+    const quiz = body.results.random(); // Get a random trivia question from a larger selection.
+    const choices = quiz.incorrect_answers.map(answ => h.decode(answ)); // Insert all the incorrect answers to the choices array.
+    choices.push(h.decode(quiz.correct_answer)); // Push the correct answer to the array as well.
     
-    a = await questions.random();
-    questions.splice(questions.indexOf(a), 1);
-    b = questions.random();
-    questions.splice(questions.indexOf(b), 1);
-    c = questions.random();
-    questions.splice(questions.indexOf(c), 1);
-    d = questions[0];
+    const randomChoices = [];
+    for (let i = 0; i < 4; i++) {
+      randomChoices[i] = choices.random(); // Select a random answer,
+      choices.splice(choices.indexOf(randomChoices[i]), 1); // Remove it from the array.
+    } // Repeat until complete
+    const [a, b, c, d] = randomChoices;
 
-    emb.setAuthor("Misaki Trivia", this.client.user.displayAvatarURL());
-    emb.setDescription(B64.decode(ob.results[0].question).replace(/â/g,"").replace(/â/g,""));
-    emb.setColor("#FC963B");
-    emb.addField("A", a);
-    emb.addField("B", b);
-    emb.addField("C", c);
-    emb.addField("D", d);
+    const emb = new Discord.MessageEmbed() // Embed it
+      .setAuthor("Misaki Trivia", this.client.user.displayAvatarURL())
+      .setColor("#FC963B")
+      .setDescription(h.decode(quiz.question))
+      .addField("A", a)
+      .addField("B", b)
+      .addField("C", c)
+      .addField("D", d);
 
-    const question = await this.client.awaitReply(message, null, m => m.author.id === message.author.id, 60000, {embed:emb});
+    const question = await this.client.awaitReply(message, null, m => m.author.id === message.author.id, 60000, {embed:emb}); // Ask the question.
+
+    let choice;
+    if (!question) return message.reply("you took too long to respond.");
+    if (!/^(a|b|c|d)$/i.test(question)) return message.reply("that isn't a valid choice..");
+    switch (question.toLowerCase()) {
+      case "a":
+        choice = a;
+        break;
+      case "b":
+        choice = b;
+        break;
+      case "c":
+        choice = c;
+        break;
+      case "d":
+        choice = d;
+        break;
+    }
     
-    if (question === false) return message.reply("you took too long to respond.");
-    if (question.toLowerCase() === "a") choice = a;
-    if (question.toLowerCase() === "b") choice = b;
-    if (question.toLowerCase() === "c") choice = c;
-    if (question.toLowerCase() === "d") choice = d;
-    if (!["a", "b", "c", "d"].includes(question.toLowerCase())) return message.reply("that isn't a valid choice..");
-    
-    if (choice === B64.decode(ob.results[0].correct_answer)) { 
-      message.member.givePoints(10); return message.reply(`that is correct! You won 10${message.settings.uEmoji}`); 
+    if (choice === quiz.correct_answer) { 
+      message.member.givePoints(10); 
+      return message.reply(`that is correct! You won 10${message.settings.uEmoji}`); 
     } else { 
       return message.reply("that is incorrect!"); 
     }
