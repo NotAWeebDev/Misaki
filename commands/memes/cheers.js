57deab720d50166c1462e51d9b4bc66cfaf7ce00
York <aarghgames@gmail.com>
2018-01-26 13:17:26 +0000

    Updated Meme commands

--- commands/memes/cheers.js
+++ commands/memes/cheers.js
@@ -15,15 +15,29 @@ class Cheers extends Meme {
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const text = args.join(" ");
     if (text.length < 5) return message.response(undefined, `Invalid Command usage: \`${this.help.usage}\``);
-    const msg = await message.channel.send(`<a:typing:397490442469376001> **${message.member.displayName}** raises their glass...`);
-    
-    await message.buildEmbed()
-      .setColor(message.guild.member(this.client.user.id).highestRole.color || 0)
-      .setImage(await this.twoMeme(5496396, text))
-      .setFooter(`Requested by ${message.member.displayName}`, message.author.displayAvatarURL())
-      .setTimestamp()
-      .send();
-    await msg.delete();
+    try {
+      if (message.settings.socialSystem === "true") {
+        if (!(await this.cmdPay(message, message.author.id, this.help.cost))) return;
+      }
+      const msg = await message.channel.send(`<a:typing:397490442469376001> **${message.member.displayName}** raises their glass...`);
+      const meme = await this.twoMeme(5496396, text);
+      await msg.edit({
+        embed: {
+          "title": "Click here if the image failed to load",
+          "url": meme,
+          "color": message.guild.member(this.client.user.id).highestRole.color || 5198940,
+          "image": {
+            "url": meme
+          },
+          "footer": {
+            "icon_url": message.author.displayAvatarURL(),
+            "text": `Requested by ${message.member.displayName}`
+          },
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 module.exports = Cheers;
--- commands/memes/skeptical.js
+++ commands/memes/skeptical.js
@@ -1,25 +1,26 @@
 const Meme = require(`${process.cwd()}/base/Meme.js`);
 
-class Brain extends Meme {
+class Notsure extends Meme {
   constructor(client) {
     super(client, {
-      name: "brain",
-      description: "Blow your mind",
-      usage: "brain <first text ; second text ; third text ; forth text>",
+      name: "skeptical",
+      description: "Express your skepticism with Philip J Fry!",
+      usage: "skeptical <top text ; bottom text>",
       category: "meme",
       cost: 5,
-      aliases: []
+      aliases: ["fry", "notsure"]
     });
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const text = args.join(" ");
+    if (text.length < 5) return message.response(undefined, `Invalid Command usage: \`${this.help.usage}\``);
     try {
       if (message.settings.socialSystem === "true") {
         if (!(await this.cmdPay(message, message.author.id, this.help.cost))) return;
       }
-      const msg = await message.channel.send(`<a:typing:397490442469376001> **${message.member.displayName}** reveals their inner knowledge...`);
-      const meme = await this.fourMeme(93895088, text);
+      const msg = await message.channel.send(`<a:typing:397490442469376001> **${message.member.displayName}** isn't sure...`);
+      const meme = await this.twoMeme(61520, text);
       await msg.edit({
         embed: {
           "title": "Click here if the image failed to load",
@@ -39,4 +40,4 @@ class Brain extends Meme {
     }
   }
 }
-module.exports = Brain;
+module.exports = Notsure;
