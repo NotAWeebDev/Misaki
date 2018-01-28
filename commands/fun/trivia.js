b9163fc3578e7266c12654fb675e39da62edb6bb
AndrewJWin <fastroute66@live.com>
2018-01-27 20:26:40 -0600

    Kyra update v2

--- commands/fun/trivia.js
+++ commands/fun/trivia.js
@@ -1,8 +1,7 @@
 const Social = require(`${process.cwd()}/base/Social.js`);
 const Discord = require("discord.js");
-const html = require("html-entities").AllHtmlEntities;
 const snek = require("snekfetch");
-const h = new html();
+const h = new (require("html-entities").AllHtmlEntities)(); // HTML encoding decoder
 
 class Trivia extends Social {
   constructor(client) {
@@ -11,7 +10,7 @@ class Trivia extends Social {
       description: "Delivers a quiz with the correct answer awarding 10 points.",
       category: "Fun",
       usage: "trivia",
-      extended: "Ready to wrack your brain? ",
+      extended: "Ready to wrack your brain?",
       cooldown: 10,
       aliases: ["quiz"],
       botPerms: ["EMBED_LINKS"]
@@ -23,49 +22,33 @@ class Trivia extends Social {
     const quiz = body.results.random(); // Get a random trivia question from a larger selection.
     const choices = quiz.incorrect_answers.map(answ => h.decode(answ)); // Insert all the incorrect answers to the choices array.
     choices.push(h.decode(quiz.correct_answer)); // Push the correct answer to the array as well.
-    
+
     const randomChoices = [];
     for (let i = 0; i < 4; i++) {
       randomChoices[i] = choices.random(); // Select a random answer,
       choices.splice(choices.indexOf(randomChoices[i]), 1); // Remove it from the array.
     } // Repeat until complete
-    const [a, b, c, d] = randomChoices;
 
     const emb = new Discord.MessageEmbed() // Embed it
       .setAuthor("Misaki Trivia", this.client.user.displayAvatarURL())
-      .setColor("#FC963B")
+      .setColor(message.settings.embedColor)
       .setDescription(h.decode(quiz.question))
-      .addField("A", a)
-      .addField("B", b)
-      .addField("C", c)
-      .addField("D", d);
+      .addField("A", randomChoices[0])
+      .addField("B", randomChoices[1])
+      .addField("C", randomChoices[2])
+      .addField("D", randomChoices[3]);
 
     const question = await this.client.awaitReply(message, null, m => m.author.id === message.author.id, 60000, {embed:emb}); // Ask the question.
 
-    let choice;
-    if (!question) return message.reply("you took too long to respond.");
-    if (!/^(a|b|c|d)$/i.test(question)) return message.reply("that isn't a valid choice..");
-    switch (question.toLowerCase()) {
-      case "a":
-        choice = a;
-        break;
-      case "b":
-        choice = b;
-        break;
-      case "c":
-        choice = c;
-        break;
-      case "d":
-        choice = d;
-        break;
-    }
-    
-    if (choice === quiz.correct_answer) { 
+    if (!question) return message.reply("you took too long to respond."); // Check against time it took. 1 Minute by default.
+    const choice = randomChoices[["a", "b", "c", "d"].indexOf(question.toLowerCase())]; // Assign correct value to "choice" from randomChoices.
+    if (!choice) return message.reply("that isn't a valid choice.."); // Check against incorrect values, correct is A, B, C and D.
+
+    if (choice === h.decode(quiz.correct_answer)) { 
       message.member.givePoints(10); 
       return message.reply(`that is correct! You won 10${message.settings.uEmoji}`); 
-    } else { 
-      return message.reply("that is incorrect!"); 
     }
+    return message.reply("that is incorrect!"); // Throw if choice !== correct answer
   
   }
 }
