a2786c866b8553803411aa3708d44ba3e6d6ae84
York <aarghgames@gmail.com>
2018-01-29 22:33:15 +0000

    Lizard Squad

---
+++ commands/animals/lizard.js
@@ -0,0 +1,43 @@
+const Social = require(`${process.cwd()}/base/Social.js`);
+const snek = require("snekfetch");
+
+class Lizard extends Social {
+  constructor(client) {
+    super(client, {
+      name: "lizard",
+      description: "Post a randomly selected image of a bird.",
+      category: "NSFW",
+      usage: "lizard",
+      extended: "This command will return a beautiful lizard.",
+      cost: 5,
+      cooldown: 10,
+      aliases: []
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    try {
+
+      if (message.settings.socialSystem === "true") {
+        if (!(await this.cmdPay(message, message.author.id, this.help.cost))) return;
+      }
+
+      const msg = await message.channel.send(`<a:typing:397490442469376001> **${message.member.displayName}** is looking for a lizard...`);
+      const { body } = await snek.get("https://nekos.life/api/lizard");
+      await msg.edit({
+        embed: {
+          "title": "Click here if the image failed to load.",
+          "url": body.url,
+          "color": message.guild.me.roles.highest.color || 5198940,
+          "image": {
+            "url": body.url
+          }
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = Lizard;
