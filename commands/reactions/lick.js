c239773b0d8ee1b105ece6896583bbc2408c4714
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2018-02-04 09:51:31 +0000

    Update

--- commands/reactions/lick.js
+++ commands/reactions/lick.js
@@ -15,6 +15,7 @@ class Lick extends Social {
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const target = message.mentions.members;
     if (target.size === 0) return message.response(undefined, "You need to mention someone to lick them.");
+    if (message.member == target.first()) return message.reponse(undefined, "You cannot lick yourself !");
     try {
       if (message.settings.socialSystem === "true") {
         if (!(await this.cmdPay(message, message.author.id, this.help.cost))) return;
