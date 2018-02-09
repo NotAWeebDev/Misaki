08862144c837056320954fc69310e2138bdada45
August (Chris) <27751995+ohlookitsAugust@users.noreply.github.com>
2018-02-08 21:10:50 -0700

    Instead of an function, do it in the command class.

--- commands/utility/reminder.js
+++ commands/utility/reminder.js
@@ -32,7 +32,7 @@ class Reminder extends Command {
       if (reminders.length === 0) return message.response(undefined, "You do not have any reminders set.");
       else return message.channel.send("**Your Reminders:**\n" + reminders.map(r => `${r.reminder} - ${moment(r.reminderTimestamp).fromNow()}`).join('\n'));
     }
-    const blah = await regCheck(args.join(" "));
+    const blah = await this.regCheck(args.join(" "));
     if (!blah) return message.respond(undefined, "Invalid Command usage, you must supply a reminder message and duration e.g; `Do the laundry in 20 minutes`.");
     this.client.reminders.set(`${message.author.id}-${message.createdTimestamp + ms(blah.split("#")[1])}`, {
       id: message.author.id,
@@ -42,6 +42,18 @@ class Reminder extends Command {
     });
 
     message.channel.send(`I will remind you to \`${blah.split("#")[0]}\`, ${blah.split("#")[1]} from now.`);
+  }
+  
+  regCheck(reminder) {
+   const remind = /(?:^| )(?:in ?)?(((?:\d{1,2}(?:\.\d|\d)?)|a) ?((?:m(?:in(?:ute)?)?|h(?:our)?|d(?:ay)?|w(?:eek)?|m(?:onth)?|y(?:ear)?)s?))\b/gi.exec(reminder);
+    if (!remind) return false;
+    const time = remind[1]
+      .replace(/ ms?\b/, " min") //m => min
+      .replace(/\ba ?((?:m(?:in(?:ute)?)?|h(?:our)?|d(?:ay)?|w(?:eek)?|m(?:onth)?|y(?:ear)?)s?)\b/g, "1 $1").trim(); // a "something" => 1 "something"
+    const input = /(?:me ?)?(?:to ?)?(.*)/gi.exec(reminder)[1]
+      .replace(remind[0], "").trim();
+    if (input.length === 0) return false;
+    return `${input}#${time}`;
   }
 }
 
