22322b8063a057b99241d0c870fa9e2138b6697e
Hutch Moore <tech_hutch@verizon.net>
2018-02-14 13:55:53 -0500

    Fixes to responses
    Grammar fixes, gender neutrality, consistent spelling.
    Line 3: Politeness.
    Line 26: Assuming the guild name, by mistake?

--- assets/responses.js
+++ assets/responses.js
@@ -1,48 +1,48 @@
 // Member Join
 exports.welcomeMessages = [
-  "O-ohayou **{{user}}-san**.",
-  "Hmph, looks like **{{user}}**, another baka has joined.",
+  "O-ohayou gozaimasu **{{user}}-san**.",
+  "Hmph, looks like **{{user}}**, another baka, has joined.",
   "It's not like I like you or anything... but welcome to the server **{{user}}**.",
   "O-oh, welcome **{{user}}-san**. I hope I haven't embarrased you...",
   "OwO who's this? **{{user}}** has joined the server.",
-  "**{{user}}**, looks like you landed on the wrong side of discord!",
+  "**{{user}}**, looks like you landed on the wrong side of Discord!",
   "Hi **{{user}}**! Welcome to **{{guild}}**, I hope you enjoy your stay!",
   "ERGH, why has **{{user}}** got to come, I already have to handle **{{amount}}** users!",
-  "Y-yay! more people to deal with! Welcome **{{user}}**!",
-  "Hi! Waifu **{{user}}** has joined the Academy!",
-  "**{{user}}-san**, Welcome to **{{guild}}**!"
+  "Y-yay! More people to deal with! Welcome **{{user}}**!",
+  "Hi! Waifu/husbando **{{user}}** has joined the Academy!",
+  "**{{user}}-san**, welcome to **{{guild}}**!"
 ];
 
 // Member Leave
 exports.goodbyeMessages = [
   "Awh, I-I have no words for t-this, b-but, I-I'll miss you **{{user}}-san**!",
   "Awh, **{{user}}** has left... n-not that I care! Baka!",
-  "**{{user}}-san**.... N-no..",
+  "**{{user}}-san**.... N-no...",
   "**{{user}}** left, not like we needed them or anything.",
   "**{{user}}** why would you leave me here, alone!?",
   "**{{user}}** discovered the leave button.",
-  "Eep! **{{user}}** left?! H-hes always leaving me, that baka..",
+  "Eep! **{{user}}** left?! Th-they're always leaving me, that baka...",
   "Finally, I thought **{{user}}** would never leave!",
-  "Woah! **{{user}}** left the Academy! b-baka, Why did they even join?!",
+  "Woah! **{{user}}** left **{{guild}}**! B-baka, why did they even join?!",
   "Woo, **{{user}}** left, now I only have to handle **{{amount}}** users!"
 ];
 
 // Level Up
 exports.levelUpMessages = [
   "You've only become level **{{level}}**, peasant!",
-  "YATTA! You leveled up **{{user}}-san**! You're now level **{{level}}**, n-not that it means a-anything!",
-  "E-eh? You levelled up? Well just because you're level **{{level}}** now, doesn't mean I'll like you more **{{user}}**... baka.",
-  "Hmph, you leveled up to **{{level}}** **{{user}}-san**, w-well it makes no difference to me!",
+  "YATTA! You leveled up, **{{user}}-san**! You're now level **{{level}}**, n-not that it means a-anything!",
+  "E-eh? You leveled up? Well just because you're level **{{level}}** now, doesn't mean I'll like you more **{{user}}**... baka.",
+  "Hmph, you leveled up to **{{level}}**, **{{user}}-san**. W-well, it makes no difference to me!",
   "Hyaa~! You've leveled up **{{user}}-san**, you're now level **{{level}}**.",
-  "**{{user}}** you baka. Don't leave me behind now that you're level **{{level}}**.",
-  "**{{user}}-san**, don't forget me now because you're level **{{level}}**",
+  "**{{user}}**, you baka. Don't leave me behind now that you're level **{{level}}**.",
+  "**{{user}}-san**, don't forget me now just because you're level **{{level}}**",
   "Pfft, you've only just become level **{{level}}**, hurry up **{{user}}**.",
   "Ah nuts, **{{user}}** is getting smart, now with an I.Q. of **{{level}}**",
   "N-nani? You've reached level **{{level}}** so quickly...",
   "You're only level **{{level}}**? You disgust me!",
-  "How can you be **{{level}}** y-you baka!",
-  "B-baka! You're at level **{{level}}**, now go be free! d-dummy...",
-  "Your definitely level **{{level}}** you baka!",
+  "How can you be level **{{level}}** y-you baka!",
+  "B-baka! You're at level **{{level}}**, now go be free! D-dummy...",
+  "You're definitely level **{{level}}** you baka!",
 ];
 
 // Commands
@@ -52,26 +52,26 @@ exports.pingMessages = [
   "H-here you go, I responded in **{{ms}}ms**.",
   "Here you go, not that it was worth my time. It only took me **{{ms}}ms**.",
   "Is this right? I've responded in **{{ms}}ms**.",
-  "**{{user}}**?, I've responded in **{{ms}}ms**.",
+  "**{{user}}**? I've responded in **{{ms}}ms**.",
   "**{{user}}**! You wasted **{{ms}}ms** of my time, ERGH",
   "D-did I do it right? I responded in **{{ms}}ms**.",
   "**{{user}}**, yes I'm here, and it took me **{{ms}}ms** to respond!",
-  "**{{user}}** Why are you ping’ing me man! You wasted **{{ms}}ms** of my time!!",
-  "**{{user}}** Hey, it took me **{{ms}}ms** to send this message",
+  "**{{user}}** why are you pinging me man! You wasted **{{ms}}ms** of my time!!",
+  "**{{user}}** hey, it took me **{{ms}}ms** to send this message",
   "You've made me **{{ms}}ms** older - just from asking.",
   "**{{user}}** I've seen your message and it took me **{{ms}}ms** not to care.",
-  "Do you know how long it took to me to read that message? You pretty much wasted **{{ms}}ms** of my day!",
+  "Do you know how long it took me to read that message? You pretty much wasted **{{ms}}ms** of my day!",
   "B-baka! My ping is **{{ms}}ms**! Are you happy now?"
 ];
 
 exports.myLevelMessages = [
-  "You're only level **{{level}}**, that makes you a **{{friendly}}.**",
-  "Pfft, level **{{level}}**? I've seen people with a better role than **{{friendly}}.**",
+  "You're only level **{{level}}**, that makes you a **{{friendly}}**.",
+  "Pfft, level **{{level}}**? I've seen people with a better role than **{{friendly}}**.",
   "**{{user}}** your level? It's **{{level}}** silly, oh the name? **{{friendly}}**, sorry...",
   "**{{user}}**, again with this? Whatever, it's level **{{level}}**, **{{friendly}}**.",
   "**{{user}}-san** here you go, it's **{{friendly}}**, level **{{level}}**.",
   "You're a **{{level}}**, maybe if you were a wee bit higher than a **{{friendly}}** you'd stop forgetting.",
-  "Only a level **{{level}}** even I'm better than a **{{friendly}}**.",
+  "Only a level **{{level}}**? Even I'm better than a **{{friendly}}**.",
   "Level **{{level}}**? What, where you hoping to be higher than **{{friendly}?",
   "smh, a level **{{level}}**? I thought you weren't gonna be a **{{friendly}}**!",
   "Only level **{{level}}**? Pfft! I've seen much better than a **{{friendly}}**."
@@ -82,8 +82,8 @@ exports.dailySuccessMessages = [
   "Well done, you've redeemed your daily **{{amount}}**!",
   "Finally I thought you were never going to claim your **{{amount}}** today!",
   "Your dad gave you **{{amount}}**? I get more than that every hour!",
-  "You have claimed your daily **{{amount}}**, Ain't that dandy?",
-  "N-nani?! You got **{{amount}}** woah...",
+  "You have claimed your daily **{{amount}}**, ain't that dandy?",
+  "N-nani?! You got **{{amount}}**, woah...",
   "You just got **{{amount}}**? Maybe buy me dinner some time **{{user}}-san**? :wink:",
   "Oh goody, you finally got your **{{amount}}**. It's about time, now get me some Pocky."
 ];
@@ -105,45 +105,45 @@ exports.balanceMessages = [
 ];
 
 exports.otherBalanceMessages = [
-  "Why do you want to know **{{user}}**'s Balance? Whatever, it's **{{amount}}** anyway.",
-  "N-nani? **{{user}}-san**'s Balance? It's **{{amount}}**..",
-  "Why do you care so much about **{{user}}**'s Balance? N-not that I care, it's **{{amount}}**.",
-  "Some particular reason you want to know **{{user}}**'s balance? it's **{{amount}}**.",
-  "Hm, one second.. It's **{{amount}}**, but why do you want to know **{{user}}**'s Balance?",
+  "Why do you want to know **{{user}}**'s balance? Whatever, it's **{{amount}}** anyway.",
+  "N-nani? **{{user}}-san**'s balance? It's **{{amount}}**..",
+  "Why do you care so much about **{{user}}**'s balance? N-not that I care, it's **{{amount}}**.",
+  "Some particular reason you want to know **{{user}}**'s balance? It's **{{amount}}**.",
+  "Hm, one second... It's **{{amount}}**, but why do you want to know **{{user}}**'s balance?",
   "You've aleady asked for **{{user}}**'s balance, gosh! Anyway, it's **{{amount}}**."
 ];
 
 exports.levelMessages = [
   "You're only level **{{level}}**, Not bad...",
   "Pfft, level **{{level}}**?",
-  "**{{user}}** your level? It's **{{level}}** baka",
-  "**{{user}}**, again with this? Whatever, your level **{{level}}**.",
+  "**{{user}}** your level? It's **{{level}}**, baka",
+  "**{{user}}**, again with this? Whatever, you're level **{{level}}**.",
   "**{{user}}-san** here you go, it's **{{level}}**.",
   "You're level **{{level}}**, maybe if you were a wee bit more active it'd be higher.",
-  "Only a level **{{level}}** even I'm better than that.",
+  "Only a level **{{level}}**? Even I'm better than that.",
   "Level **{{level}}**? What, where you hoping to be higher than **{{level}}**?",
   "Only level **{{level}}**? Pfft! I've seen much better than that."
 ];
 
 exports.otherLevelMessages = [
-  "Look just ask **{{user}}** next time, but I will forgive you, here this is his level **{{level}}**.",
+  "Look just ask **{{user}}** next time, but I will forgive you. Here, this is their level: **{{level}}**.",
   "Why do you want to view **{{user}}**'s level? Anyway they are level **{{level}}**."
 ];
 
 exports.aboutMessages = [
-  "You want to know about me? What don't you already know b-baka. I get my beautiful looks from `@VixtoriaGoober` (Follow her on Twitter) and I'm the entertainer for An Idiot's Guide, owned by my father York#2400... I can tell you anything about any Anime or Manga that I have, not that it m-matters to me. Look, I'm also asked to give you a daily allowance and keep a record of what everyone else has. Will you just go with it?",
+  "You want to know about me? What don't you already know b-baka. I get my beautiful looks from `@VixtoriaGoober` (Follow her on Twitter) and I'm the entertainer for An Idiot's Guide, owned by my father York#2400... I can tell you anything about any anime or manga that I have, not that it m-matters to me. Look, I'm also asked to give you a daily allowance and keep a record of what everyone else has. Will you just go with it?",
 
-  "I'm Misaki! Your entertainment assistant for An Idiot's Guide, a fantastic place owned by my Father. I have a wide range of Manga and Anime, if you ever have a question or just want to look at it; all you need to do is ask. I also help out with keeping the community up by providing allowances for everyone! I've even kept a track of everyones balance so you can see where you are, even though you're always at the top for me!\n\np.s. I get my beautiful looks from `@VixtoriaGoober` (Follow her on Twitter)"
+  "I'm Misaki! Your entertainment assistant for An Idiot's Guide, a fantastic place owned by my father. I have a wide range of manga and anime; if you ever have a question or just want to look at it, all you need to do is ask. I also help out with keeping the community up by providing allowances for everyone! I've even kept track of everyone's balance so you can see where you are, even though you're always at the top for me!\n\nP.S. I get my beautiful looks from `@VixtoriaGoober` (follow her on Twitter)"
 ];
 
 exports.rebootMessages = [
   "I'm just leaving for a bit... W-what are you? O-obsessed?",
-  "I'm feeling a bit tired, better get some sleep **Goodnight**",
+  "I'm feeling a bit tired, better get some sleep. **Goodnight**",
   "S-senpai please wait for me...",
   "B-baka! You need to wait for me...",
-  "W-what, **{{user}}**, You want to watch me sleep? P-pervert.",
-  "**{{user}}-san**! I'm restarting, give me a moment..",
-  "Hey! **{{user}}**, what are you doing, WAIT!?!"
+  "W-what, **{{user}}**, you want to watch me sleep? P-pervert.",
+  "**{{user}}-san**! I'm restarting, give me a moment...",
+  "Hey! **{{user}}**, what are you doing!?! WAIT!!"
 ];
 
 // Go Messages
@@ -159,24 +159,24 @@ exports.goMessages = [
 // Reboot
 exports.bootOneMessages = [
   "**{{user}}-san**! I'm back!",
-  "**{{user}}**, Why did you do that?"
+  "**{{user}}**, why did you do that?"
 ];
 
 exports.bootTwoMessages = [
   "**{{user}}-san**, did you miss me? I only took **{{ms}}ms**.",
-  "**{{user}}**, I only got {{ms}} of sleep"
+  "**{{user}}**, I only got **{{ms}}ms** of sleep"
 ];
 
 // Reload
 exports.reloadMissingArg = [
   "N-nani?! I don't know what to do, you didn't give me anything!",
-  "Baka! You are suppossed to provide a command to reload!"
+  "Baka! You are supposed to provide a command to reload!"
 ];
 
 exports.reloadNotFound = [
   "Eeeh?! **{{user}}-san**, why would you ask me to find a command that doesn't exist, it wasn't even an alias.",
   "What are you doing? **{{command}}** doesn't exist! Baka!"
-];  
+];
 
 exports.reloadErrUnload = [
   "**{{user}}-san**, I think I broke it... This happened: **{{response}}**",
