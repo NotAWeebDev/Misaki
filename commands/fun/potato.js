70399662a735aac87a934d02776ffe65f957f692
York <YorkAARGH@users.noreply.github.com>
2018-02-16 13:32:42 +0000

    Update potato.js

--- commands/fun/potato.js
+++ commands/fun/potato.js
@@ -1,4 +1,5 @@
 const Social = require(`${process.cwd()}/base/Social.js`);
+const { get } = require("snekfetch");
 
 class Potato extends Social {
   constructor(client) {
@@ -14,14 +15,14 @@ class Potato extends Social {
   }
 
   async run(message, args, level, loadingMessage) { // eslint-disable-line no-unused-vars
-    const potato = await this.cmdWeeb("potato");
+    const { body } = await get("https://rra.ram.moe/i/r?type=potato");
     await loadingMessage.edit({
       embed: {
         "title": "Click here if the image failed to load.",
-        "url": `https://cdn.ram.moe/${potato}`,
+        "url": `https://cdn.ram.moe/${body.path.replace("/i/", "")}`,
         "color": message.guild.me.roles.highest.color || 5198940,
         "image": {
-          "url": `https://cdn.ram.moe/${potato}`
+          "url": `https://cdn.ram.moe/${body.path.replace("/i/", "")}`
         }
       }
     });
