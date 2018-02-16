3e470ad6b1b615d7f7ae01bd35718e5fd3e3d6c0
AndrewJWin <fastroute66@live.com>
2018-02-16 10:48:30 -0600

    Jacz changes

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,5 +1,5 @@
 const Command = require(`${process.cwd()}/base/Command.js`);
-const {MessageEmbed} = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const perpage = 10;
 /*
   The HELP command is used to display every command's name and description
@@ -18,7 +18,7 @@ class Help extends Command {
     });
   }
 
-  async run(message, args, level) {
+  async run(message, [type, page], level) {
 
     const embed = new MessageEmbed()
       .setTimestamp()
@@ -29,8 +29,8 @@ class Help extends Command {
     // Here we sort out categories in case the user did not provide an argument.
     
     let currentCategory = "";
-    const sorted = this.client.commands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
-    if (!args[0]) {
+    const sorted = this.client.commands.sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+    if (!type) {
       const description = `Command category list\n\nUse \`${message.settings.prefix}help <category>\` to find commands for a specific category`;
       let output = "";
       sorted.forEach( c => {
@@ -38,7 +38,7 @@ class Help extends Command {
         if (c.help.category === "NSFW" && !message.channel.nsfw) return;
 
         const cat = c.help.category.toProperCase();
-        if (currentCategory !== cat && !args[0]) {
+        if (currentCategory !== cat && !type) {
           output += `\`${message.settings.prefix}help ${cat.toLowerCase()}\` | Shows ${cat} commands\n`;
           currentCategory = cat;
         }
@@ -47,23 +47,23 @@ class Help extends Command {
       embed.setDescription(description)
         .addField("Categories", output);
 
-      message.channel.send({embed});
+      return message.channel.send({embed});
     } else {
       let lol = 0;
       sorted.forEach(c => {
-        if (c.help.category.toLowerCase() == args[0]) {
+        if (c.help.category.toLowerCase() == type) {
           lol = lol + 1;
         }
       });
     
       let output = "";
       let num = 0;
-      const page = parseInt(args[1]) > 0 && parseInt(args[1]) <= Math.ceil(lol / perpage) ? parseInt(args[1]) : 1;
+      const pg = parseInt(page) > 0 && parseInt(page) <= Math.ceil(lol / perpage) ? parseInt(page) : 1;
       sorted.forEach(c => {
-        if (c.help.category.toLowerCase() == args[0]) {
+        if (c.help.category.toLowerCase() == type) {
           if (c.help.category === "Owner" && level < 10 ) return;
           if (c.help.category === "NSFW" && !message.channel.nsfw) return;
-          if (num < perpage * page && num > perpage * page - (perpage + 1)) {
+          if (num < perpage * pg && num > perpage * pg - (perpage + 1)) {
             if (level < this.client.levelCache[c.conf.permLevel]) return;
             output += `\n\`${message.settings.prefix + c.help.name}\` | ${c.help.description.length > 50 ? c.help.description.slice(0,50) +"...": c.help.description}`;
           }
@@ -71,26 +71,26 @@ class Help extends Command {
         }
       });
     
-      if (num != 0) {
+      if (num) {
         //message.channel.send(`${title}\n\n${description}\n${output}`, {code:"asciidoc"});
         embed.setTitle("Command category help")
-          .setDescription(`A list of commands in the ${args[0]} category.  (Total of ${num} commands in this category)\n\nTo get help on a specific command do \`${message.settings.prefix}help <command>\`\n\n${num > 10 && page === 1 ? `To view more commands do\` ${message.settings.prefix}help <category> 2\`` : "" }`)
+          .setDescription(`A list of commands in the ${type} category.  (Total of ${num} commands in this category)\n\nTo get help on a specific command do \`${message.settings.prefix}help <command>\`\n\n${num > 10 && page === 1 ? `To view more commands do\` ${message.settings.prefix}help <category> 2\`` : "" }`)
           .addField("Commands", output);
 
-        message.channel.send({embed});
+        return message.channel.send({embed});
       }
     }
     // Show individual command's help.
-    let command = args[0];
+    let command = type;
     if (this.client.commands.has(command) || this.client.commands.forEach(command => {if (command.conf.aliases.includes(command)) return true;})) {
       command = this.client.commands.get(command);
       if (level < this.client.levelCache[command.conf.permLevel]) return;
-      embed.setTitle(`${args[0]} help`)
+      embed.setTitle(`${type} help`)
         .addField("Command description", command.help.description)
         .addField("Command usage", `\`${command.help.usage}\``)
         .addField("Command aliases", command.conf.aliases.length == 0 ? "None" : command.conf.aliases.join(", ") );
     
-      message.channel.send({embed});
+      return message.channel.send({embed});
     }
   }
 }
