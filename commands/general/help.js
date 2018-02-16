ae5746b4ac4b32eabceafa2f02c5326074a3ca3b
AndrewJWin <fastroute66@live.com>
2018-02-16 15:44:54 -0600

    Yukine's turn (#63)
    * All one embed!
    
    Yaaaa
    
    * Cleaned up spacing
    
    Oops, little spacing issues.
    
    * Colors
    
    * Requested Changed
    
    As well as a couple improvements. (Keep your hands off Jacz.)
    
    * Quick changes
    
    * Fixed issues
    
    Along with various safeguards, will not post Owner commands to users with permLevel < 10, will not post NSFW commands in a non-NSFW channel, as well as removing commands from categories where the user's level does not match the required level.
    
    * Jacz changes
    
    * Cleaned up
    
    Refactored (slightly)
    
    * Update help.js
    
    * Yukine's Fixes
    
    * Update help.js
    
    * Update help.js
    
    * Last update
    
    🤦
    
    * Add files via upload
    
    * Final
    
    * Fix

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,55 +1,91 @@
 const Command = require(`${process.cwd()}/base/Command.js`);
-
+const { MessageEmbed } = require("discord.js");
+const perpage = 10;
+/*
+  The HELP command is used to display every command's name and description
+  to the user, so that he may see what commands are available. The help
+  command is also filtered by level, so if a user does not have access to
+  a command, it is not shown to them. If a command name is given with the
+  help command, its extended help is shown.
+*/
 class Help extends Command {
   constructor(client) {
     super(client, {
       name: "help",
-      description: "Displays the commands for your level.",
-      usage: "help [command]",
-      category: "System",
+      description: "Get help on a command, command category, or a setting",
       extended: "This command will display all available commands for your permission level, with the additonal option of getting per command information when you run 'help <command name>'.",
-      hidden: true,
+      category: "System",
+      usage: "help <category/command/setting> [page-num]",
       aliases: ["h", "halp", "commands"]
     });
   }
 
-  async run(message, args, level) {
-    const settings = message.settings;
-    if (!args[0]) {
-      try {
-        const myCommands = message.guild ? this.client.commands.filter(cmd => this.client.levelCache[cmd.conf.permLevel] <= level && cmd.conf.hidden !== true) : this.client.commands.filter(cmd => this.client.levelCache[cmd.conf.permLevel] <= level && cmd.conf.hidden !== true && cmd.conf.guildOnly !== true);
-        const commandNames = myCommands.keyArray();
-        const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-        let currentCategory = "";
-        let output = `= Command List =\n\n[Use ${settings.prefix}help <commandname> in a guild channel for details]\n`;
-        const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
-        sorted.forEach( c => {
-          const cat = c.help.category.toProperCase();
-          if (currentCategory !== cat) {
-            output += `\u200b\n== ${cat} ==\n`;
-            currentCategory = cat;
-          }
-          output += `${settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
-        });
-        await message.channel.send("Please check your inbox for a list of my commands.");
-        await message.author.send(output, {code:"asciidoc", split: { char: "\u200b" }});
-      } catch (error) {
-        if (error.message === "Cannot send messages to this user") {
-          return message.reply("I cannot send you the commands message, as it appears you have **Direct Messages's** disabled.");
-        } else {
-          throw error;
-        }       
-      }
+  async run(message, [type, page], level) {
+
+    const embed = new MessageEmbed()
+      .setTimestamp()
+      .setColor(message.guild.me.roles.highest.color || 0x00AE86)
+      .setFooter(`Requested by ${message.author.tag}`, message.author.avatarURL()); 
+    // Preload MessageEmbed.
+  
+    // Here we sort out categories in case the user did not provide an argument.    
+    let currentCategory = "";
+    const sorted = this.client.commands.sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+    if (!type) {
+      const description = `Command category list\n\nUse \`${message.settings.prefix}help <category>\` to find commands for a specific category`;
+      const output = sorted.filter(c => !(level < 10 && c.help.category == "Owner") || !(c.help.category === "NSFW" && !message.channel.nsfw)).map(c => {
+        const cat = c.help.category.toProperCase();
+        if (currentCategory !== cat && !type) {
+          currentCategory = cat;
+          return `\n\`${message.settings.prefix}help ${cat.toLowerCase()}\` | Shows ${cat} commands`;
+        }
+      }).join("");
+
+      embed.setDescription(description)
+        .addField("Categories", output);
     } else {
-      let command = args[0];
-      
-      if (this.client.commands.has(command)) command = this.client.commands.get(command);
-      else if (this.client.aliases.has(command)) command = this.client.commands.get(this.client.aliases.get(command));
-      else return;
-      
-      if (!message.guild && command.conf.guildOnly === true) return;
-      if (level < this.client.levelCache[command.conf.permLevel]) return;
-      message.channel.send(`= ${command.help.name} = \n${command.help.description}\ncategory     :: ${command.help.category}\ncost         :: ₲${parseInt(command.help.cost)}\nusage        :: ${command.help.usage}\naliases      :: ${command.conf.aliases.join(", ")}\ndetails      :: ${command.help.extended}`, {code:"asciidoc"});    }
+      let n = 0;
+      sorted.forEach(c => {
+        if (c.help.category.toLowerCase() === type.toLowerCase()) {
+          n++;
+        }
+      });
+    
+      let output = "";
+      let num = 0;
+      const pg = parseInt(page) && parseInt(page) <= Math.ceil(n / perpage) ? parseInt(page) : 1;
+      for (const c of sorted.values()) {
+        if (c.help.category.toLowerCase() === type.toLowerCase()) {
+          if (c.help.category === "Owner" && level < 10 ) return;
+          if (c.help.category === "NSFW" && !message.channel.nsfw) return;
+          if (num < perpage * pg && num > perpage * pg - (perpage + 1)) {
+            if (level < this.client.levelCache[c.conf.permLevel]) return;
+            output += `\n\`${message.settings.prefix + c.help.name}\` | ${c.help.description.length > 50 ? c.help.description.slice(0,50) +"...": c.help.description}`;
+          }
+          num++;
+        }
+      }
+    
+      if (num) {
+        embed.setTitle("Command category help")
+          .setDescription(`A list of commands in the ${type} category.  (Total of ${num} commands in this category)\n\nTo get help on a specific command do \`${message.settings.prefix}help <command>\`\n\n${num > 10 && pg === 1 ? `To view more commands do\` ${message.settings.prefix}help <category> 2\`` : "" }`)
+          .addField("Commands", output);
+      }
+    }
+    // Show individual command's help.
+    if (this.client.commands.has(type) || this.client.commands.some(command => command.conf.aliases.includes(type))) {
+      const cm = this.client.commands.get(type) || this.client.commands.get(this.client.aliases.get(type));
+      if (level < this.client.levelCache[cm.conf.permLevel]) return;
+      embed.setTitle(cm.help.name.toProperCase())
+        .addField("Command description", cm.help.description)
+        .addField("Command usage", `\`${cm.help.usage}\``)
+        .addField("Command aliases", cm.conf.aliases.length == 0 ? "None" : cm.conf.aliases.join(", ") );
+    }
+    
+    if (!embed.fields[0]) return;
+
+    return message.channel.send(embed);
   }
 }
+
 module.exports = Help;
