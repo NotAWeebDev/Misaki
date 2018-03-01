41453a8d9c80ae4dd023f60685b174da48912ee8
Leon <leonbohm@gmail.com>
2018-03-01 15:17:58 +0000

    wow look at these descriptive commit messages

--- util/PagedEmbedUtil.js
+++ util/PagedEmbedUtil.js
@@ -1,4 +1,4 @@
-const { Message, TextChannel, GuildMember, User} = require("discord.js");
+const { MessageEmbed, GuildMember, User} = require("discord.js");
 
 const EMOJIS = {
   "1234": "🔢",
@@ -42,10 +42,8 @@ const EMOJIS = {
   reload: "🔃"
 };
 
-module.exports = class PagedEmbedUtil {
-  constructor(msgOrChan, caller) {
-
-    this.EMOJIS = EMOJIS;
+const peutil = class PagedEmbedUtil {
+  constructor(options = {}) {
 
     this.pages = [];
     this.currentPage = 0;
@@ -53,19 +51,18 @@ module.exports = class PagedEmbedUtil {
     this.responseMap = new Map(); //map, mapping emoji to action or function to be called, for example it might map ▶ to nextPage
     this.reactions = []; //array with emojis in order of how they should appear on the message
 
-    if (msgOrChan instanceof Message) {
-      this.message = msgOrChan; 
-      this.channel = msgOrChan.channel;
-    } else if (msgOrChan instanceof TextChannel) this.channel = msgOrChan;
-    else throw new TypeError("First argument must be a message or channel object!");
+    this.message = options.message || null; 
+    this.channel = options.channel || null;
     // if its a TextChannel, the message will be sent with the first page when the pagedembed is executed.
 
+    const caller = options.caller;
     if (caller instanceof GuildMember) this.caller = caller;
     if (caller instanceof User) this.caller = this.channel.guild.member(caller);
     if (typeof caller === "string") this.caller = this.channel.guild.members.get(caller);
-    if (!caller) throw new TypeError("Second argument must be the member or user who instantiated the command");
+    if (!caller) throw new TypeError("Option \"caller\" must be the member or user who instantiated the command");
 
     this.collector = null;
+    this.collectorOptions = options.collectorOptions || {}; 
   }
 
   addPage(embed) {
@@ -75,6 +72,11 @@ module.exports = class PagedEmbedUtil {
 
   addPages(embeds) {
     this.pages = this.pages.concat(embeds);
+    return this;
+  }
+
+  setPages(embeds) {
+    this.pages = embeds;
     return this;
   }
 
@@ -88,11 +90,10 @@ module.exports = class PagedEmbedUtil {
     if (position !== -1) this.reactions.splice(position, 1);
     this.reactions.push(emoji);
     if (typeof action === "string") {
-      if (["nextpage", "next"].indexOf(action) !== -1) action = this.nextPage;
-      if (["prevpage", "prev"].indexOf(action) !== -1) action = this.prevPage;
-      if (["prompt", "goto", "gotopage", "ask"].indexOf(action) !== -1) action = this.createPrompt;
+      if (["nextpage", "next"].indexOf(action) !== -1) action = this.nextPage.bind(this);
+      if (["prevpage", "prev"].indexOf(action) !== -1) action = this.prevPage.bind(this);
+      if (["prompt", "goto", "gotopage", "ask"].indexOf(action) !== -1) action = this.createPrompt.bind(this);
     }
-
     this.responseMap.set(emoji, action);
     return this;
   }
@@ -117,9 +118,9 @@ module.exports = class PagedEmbedUtil {
 
   pageTo(page) {
     if (typeof page === "string") page = Number(page);
-    if (!Number.isInteger(page)) return Promise.reject(new TypeError("Argument must be an integer"));
+    if (!Number.isInteger(page)) throw new TypeError("Argument must be an integer");
     page--; // users will assume its 1-indexed and not 0-indexed
-    if (page < 0 || page >= this.pages.length) return Promise.reject(new TypeError("Out of page range"));
+    if (page < 0 || page >= this.pages.length) throw new RangeError("Out of page range");
     this.currentPage = page;
     return this._update();
   }
@@ -135,16 +136,23 @@ module.exports = class PagedEmbedUtil {
   }
 
   async run() {
-    if (!this.message) this.message = await this.channel.send({ embed: this.pages[this.currentPage] });
+    if (!this.message) {
+      const embed = this.pages[this.currentPage];
+      if (!embed) return;
+      embed.footer = {
+        text: `${this.currentPage + 1}/${this.pages.length}` 
+      };
+      this.message = await this.channel.send({ embed: this.pages[this.currentPage] });
+    }
     for (const emoji of this.reactions) {
       await this.message.react(emoji);
     }
 
-    this.collector = this.message.createReactionCollector((reaction, user) => user === this.caller.user && this.reactions.indexOf(reaction.emoji.name) !== -1, {time: 15 * 60 * 1000});
+    this.collector = this.message.createReactionCollector((reaction, user) => user === this.caller.user && this.reactions.indexOf(reaction.emoji.name) !== -1, {time: 15 * 60 * 1000}, this.collectorOptions);
 
     this.collector.on("collect", r => {
       const perms = this.channel.permissionsFor(this.channel.guild.me).has("MANAGE_REACTIONS");
-      if (perms) r.remove(this.caller.user);
+      if (perms) r.users.remove(this.caller.user);
       const cb = this.responseMap.get(r.emoji.name);
       if (!cb) throw new TypeError(`Invalid action for emoji ${r.emoji.name}, must be a function or recognised string`);
       cb();
@@ -155,13 +163,43 @@ module.exports = class PagedEmbedUtil {
       if (perms) this.message.clearReactions();
       else this.message.reactions.map(r => r.remove());
     });
-
   }
 
   _update() {
     const embed = this.pages[this.currentPage];
-    embed.footer.text = `${this.currentPage + 1}/${this.pages.length}`; 
+    if (!embed) return;
+    embed.footer = {
+      text: `${this.currentPage + 1}/${this.pages.length}` 
+    };
     return this.message.edit({ embed });
   }
 
+  static splitFields(string, embed, perPage = 2, fieldTitle = "\u200b") {
+    
+    const split = string.split(/(\n|\.)/).reduce((acc, b, i) => {
+      if (i % 2 === 1) acc.push(b);
+      else acc[acc.length - 1] += b;
+      return acc;
+    }, []).reduce((acc, b) => {
+      if (acc[acc.length - 1].length + b.length > 1024) acc.push(b);
+      else acc[acc.length - 1] += b;
+      return acc;
+    }, []);
+
+    if (!embed) return split;
+    return split.reduce((acc, b, i) => {
+      if (acc % perPage === 1) acc.push(this.cloneEmbed(embed));
+      acc[acc.length - 1].addField(i === 1 ? fieldTitle : "\u200b");
+    }, []);
+
+
+  }
+
+  static cloneEmbed(embed) {
+    return Object.assign(new MessageEmbed(), embed);
+  }
 };
+
+peutil.EMOJIS = EMOJIS;
+
+module.exports = peutil;
