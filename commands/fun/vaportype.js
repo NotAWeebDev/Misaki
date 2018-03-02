3e499bb4730c3854755767e2bb2611578f50516b
AndrewJWin <fastroute66@live.com>
2018-03-02 15:13:06 -0600

    Fixed help command (#72)
    * Fixed filter and included Owner override
    
    * Fixes filtering and other issues
    
    Fixed a couple issues, also set up filtering for NSFW commands in a NSFW channel
    * Overridden for Owner
    For categories; commands that the user cannot use, but if there are commands available, the remaining commands can still be viewed.
    
    * Fixes
    
    * Missed an = while counting characters.
    
    * New command; Vaportype
    
    recreates the meme to:
    
    **S P A C E  E V E R Y T H I N G  O U T**
    
    * Fixes
    
    * Fixes
    
    * 🤦
    
    * Update vaportype.js
    
    * Jacz nanoseconds
    
    * Update vaportype.js

---
+++ commands/fun/vaportype.js
@@ -0,0 +1,22 @@
+const Command = require(`${process.cwd()}/base/Command.js`);
+class Vaportype extends Command {
+  constructor(client) {
+    super(client, {
+      name: "vaportype",
+      description: "W O A H  D U D E  S P A C E D  O U T",
+      usage: "vaportype <text>",
+      category: "fun",
+      aliases: ["vapor", "vpt"]
+    });
+  }
+
+  async run(message, args) {
+    if (!args.length) return message.reply("you have to provide me text to space out you baka."); // Test for any args.
+    let msg = "";
+    for (let i = 0; i < args.length; i++) msg += args[i].toUpperCase().split("").join(" ") + "  "; // Split up the arguments, then add to msg.
+    return message.channel.send(msg);
+  }
+
+}
+
+module.exports = Vaportype;
