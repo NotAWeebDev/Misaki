db75e9eb6d81f5b6070417d066d23e96d6ccd22e
Kshitij Sharma <kshitij4sharma@gmail.com>
2018-03-04 17:36:51 +0400

    Update facepalm.js

--- commands/canvas/facepalm.js
+++ commands/canvas/facepalm.js
@@ -5,29 +5,19 @@ class Facepalm extends Social {
   constructor(client) {
     super(client, {
       name: "facepalm",
-      description: "Slap another user as Batman.",
+      description: "Facepalm at someone's stupidity",
       category: "Canvas",
       usage: "facepalm",
-      extended: "Mention another user to slap them as batman.",
+      extended: "Facepalm after witnessing something very stupid.",
       cost: 10,
-      cooldown: 10
+      cooldown: 10,
+      loadingString: "<a:typing:397490442469376001> **{{displayName}}** is just lost for words..."
     });
   }
 
-  async run(message, args, level) { // eslint-disable-line no-unused-vars
-    let msg;
-    try {
-      if (message.settings.socialSystem === "true") {
-        if (!(await this.cmdPay(message, message.author.id, this.help.cost))) return;
-      }
-
-      msg = await message.channel.send(`<a:typing:397490442469376001> **${message.member.displayName}** is just lost for words...`);
-      await message.channel.send(new MessageAttachment(await this.client.idiotAPI.facepalm(message.author.displayAvatarURL({format:"png", size:256})), "facepalm.png"));
-      await msg.delete();
-    } catch (error) {
-      msg.edit("Something went wrong, please try again later");
-      this.client.logger.error(error);
-    }
+  async run(message, args, level, loadingMessage) {
+    await message.channel.send(new MessageAttachment(await this.client.idiotAPI.facepalm(message.author.displayAvatarURL({format:"png", size:256})), "facepalm.png"));
+    await loadingMessage.delete();
   }
 }
 
