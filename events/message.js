4aa890f268afb2e3af44cc63c3a18c4e516a2a20
Jacz <jacz@jaczaus.me>
2018-03-05 04:35:15 +1100

    Message Event: Add check to see if author is bot if so return

--- events/message.js
+++ events/message.js
@@ -7,6 +7,7 @@ module.exports = class {
   }
 
   async run(message) {
+    if (message.author.bot) return;
     if (message.guild && !message.guild.me) await message.guild.members.fetch(this.client.user);
     if (message.guild && !message.channel.postable) return;
 
