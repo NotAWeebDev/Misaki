965012ce4da395fc4fe1cbeb53b30f8153be0440
Kshitij Sharma <kshitij4sharma@gmail.com>
2018-03-15 18:28:49 +0400

    Create botStatsHandler.js

---
+++ util/botStatsHandler.js
@@ -0,0 +1,18 @@
+const { post } = require("snekfetch");
+
+module.exports = class BotListHandler {
+  constructor(client) {
+    this.client = client;
+    this.interval = this.client.setInterval(this.updateStats.bind(this), 9e5);
+  }
+
+  async updateStats() {
+    try {
+      await post(`https://discordbots.org/api/bots/${this.client.user.id}/stats`)
+        .set("Authorization", this.client.tokens.DBLTOKEN)
+        .send({server_count: this.client.guilds.size});
+    } catch (error) {
+      this.client.console.error(error);
+    }
+  }
+};
