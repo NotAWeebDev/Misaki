3f112fcb9d3d988a2e9af68a1243ae4d869fa2ef
Jacz <jacz@jaczaus.me>
2018-03-17 00:33:24 +1100

    Add urban from master + fix bug in urban

---
+++ commands/fun/urban.js
@@ -0,0 +1,41 @@
+const Command = require(`${process.cwd()}/base/Command.js`);
+const { MessageEmbed } = require("discord.js");
+const snek = require("snekfetch");
+
+module.exports = class UrbanCommand extends Command {
+
+  constructor(...args) {
+    super(...args, {
+      name: "urban",
+      description: "Searches the Urban Dictionary.",
+      usage: "urban <search term>",
+      aliases: ["ud"]
+    });
+  }
+
+  async run(message, [...text]) { // eslint-disable-line no-unused-vars
+    if (!text.length) return message.send(message.getText("MUST_PROVIDE_SEARCH_TERM"));
+    const { body } = await snek.get(`http://api.urbandictionary.com/v0/define?term=${text.join(" ")}`);
+    if (body.result_type === "no_result") return message.channel.send("No data found...");	
+  
+    return this.paginate(message, body.list, this.makeEmbed);
+  }
+  
+  makeEmbed(list, page) {    
+    let description = list[page].definition;
+    description += `\n\n**❯ Example**:\n${list[page].example}`;
+		
+    return new MessageEmbed()
+      .setColor(0x1e90ff)
+      .setDescription(description)
+      .setThumbnail("https://i.imgur.com/ressY86.png")
+      .setTitle(`${list[page].word} - Page ${page+1}/${list.length}`)
+      .addField(":thumbsup:", list[page].thumbs_up, true)
+      .addField(":thumbsdown:", list[page].thumbs_down, true)
+      .addBlankField(true)
+      .setURL(`${list[page].permalink}`)
+      .setFooter(`Author: ${list[page].author}`);
+  }
+  
+
+};
