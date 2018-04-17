c69145dc51610d2a673bd4e357a2145fa6d42ae1
York <aarghgames@gmail.com>
2018-04-17 11:39:19 +0100

    DBL Bad Gateway check

--- commands/social/upvote.js
+++ commands/social/upvote.js
@@ -18,18 +18,22 @@ class Upvote extends Social {
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
-    if (message.guild.id !== "396331425621868554") return message.reply(`This command can only be used in Okami Academy, run ${message.settings.prefix}invite to get the invite link.`);
-    const { body } = await get(`https://discordbots.org/api/bots/${this.client.user.id}/check?userId=${message.author.id}`).set("Authorization", process.env.DBLTOKEN);
-    if (Boolean(body.voted)) { // eslint-disable-line no-extra-boolean-cast
-      if (message.member.roles.has(upvoterRole)) return message.channel.send("You have the role already.");
-      message.member.roles.add(upvoterRole)
-        .then(() => message.reply("You have been awarded the `Updooter` role, thank you for your support.\nDon't forget to revote next month!"))
-        .catch((error) => {
-          this.client.logger.error(error);
-          message.channel.send("Something went wrong, please try again later.");
-        });
-    } else {
-      message.channel.send(`To claim the \`Updooter\` to show your support, please go upvote ${this.client.user.username} at <https://discordbots.org/bot/misaki>, then after voting return and run the command again to claim your role reward.`);
+    try {
+      if (message.guild.id !== "396331425621868554") return message.reply(`This command can only be used in Okami Academy, run ${message.settings.prefix}invite to get the invite link.`);
+      const { body } = await get(`https://discordbots.org/api/bots/${this.client.user.id}/check?userId=${message.author.id}`).set("Authorization", process.env.DBLTOKEN);
+      if (Boolean(body.voted)) { // eslint-disable-line no-extra-boolean-cast
+        if (message.member.roles.has(upvoterRole)) return message.channel.send("You have the role already.");
+        message.member.roles.add(upvoterRole)
+          .then(() => message.reply("You have been awarded the `Updooter` role, thank you for your support.\nDon't forget to revote next month!"))
+          .catch((error) => {
+            this.client.logger.error(error);
+            message.channel.send("Something went wrong, please try again later.");
+          });
+      } else {
+        message.channel.send(`To claim the \`Updooter\` to show your support, please go upvote ${this.client.user.username} at <https://discordbots.org/bot/misaki>, then after voting return and run the command again to claim your role reward.`);
+      } 
+    } catch (error) {
+      if (error.status === 502) message.channel.send(`My deepest apologes ${message.author}-san, but DBL is currently offline, please try again later.`);
     }
   }
 }
