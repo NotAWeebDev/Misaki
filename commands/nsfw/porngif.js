05a183bee3742b4a9d70da846e2c9deeef799343
York <aarghgames@gmail.com>
2018-04-22 23:05:08 +0100

    140 Commands!

---
+++ commands/nsfw/porngif.js
@@ -0,0 +1,38 @@
+const Social = require("../../structures/Social.js");
+const { get } = require("snekfetch");
+
+class PornGif extends Social {
+  constructor(...args) {
+    super(...args, {
+      name: "porngif",
+      description: "This command will return porn in gif form.",
+      category: "NSFW",
+      usage: "porngif",
+      cost: 40,
+      cooldown: 10,
+      loadingString: "<a:typing:397490442469376001> **{{displayName}}** is looking for porn gifs...",
+      aliases: ["pgif"]
+    });
+  }
+
+  async run(message, args, level, loadingMessage) { // eslint-disable-line no-unused-vars
+    if (!message.channel.nsfw) return message.response("🔞", "Cannot display NSFW content in a SFW channel.");
+    const { body } = await get("https://nekobot.xyz/api/image?type=pgif");
+    await loadingMessage.edit({
+      embed: {
+        "title": "Click here if the image failed to load.",
+        "url": body.message,
+        "color": message.guild ? message.guild.me.roles.highest.color : 5198940,
+        "image": {
+          "url": body.message
+        },
+        "footer": {
+          "icon_url": message.author.displayAvatarURL({ format: "png", size: 32 }),
+          "text": `Requested by ${message.author.tag} | Powered by NekoBot API`
+        }
+      }
+    });
+  }
+}
+
+module.exports = PornGif;
