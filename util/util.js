55af3314859726e08acaa577e3ae8fdae37f62ee
Jacz <jacknagle22@gmail.com>
2018-04-25 04:28:23 +1000

    fix reloading, remove arrDiff because its not used anymore (#96)

--- util/util.js
+++ util/util.js
@@ -7,16 +7,6 @@ class Util {
     return str.replace(Util.REGEXPESC, "\\$&");
   }
 
-  static arrDiff(a, b) {
-    if (a === b) return [];
-
-    for (const item of a) {
-      const ind = b.indexOf(item);
-      if (ind !== -1) b.splice(ind, 1);
-    }
-
-    return b;
-  }
 }
 
 Util.wait = require("util").promisify(setTimeout);
