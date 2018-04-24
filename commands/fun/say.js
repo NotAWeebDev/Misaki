f8f10b82754ee381950f11308d4cf6a3b2e9f876
York <aarghgames@gmail.com>
2018-04-24 21:32:56 +0100

    Overhauled say command
    This will close #97 due to the nature of the "overhaul" with the addition of extra filters @MrJacz don't screech over the user of missing, it works as it is without error.

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -1,11 +1,12 @@
 const Social = require("../../structures/Social.js");
+const CHANNEL_REGEX = /<#(\d{17,19})>/g;
 
 class Say extends Social {
   constructor(...args) {
     super(...args, {
       name: "say",
       description: "Make the bot say something.",
-      usage: "say [#channel] <message>",
+      usage: "say [-owo | -mock] [#channel] <message>",
       category: "Fun",
       extended: "You can send a message to another channel via this command.",
       cost: 5,
@@ -13,24 +14,46 @@ class Say extends Social {
     });
   }
 
-  async run(message, [channel, ...msg], level, loadingMessage) {
+  async cmdVerify(message, args, loadingMessage) {
+    let chan = CHANNEL_REGEX.exec(args);
+    let response;
+    if (!chan) chan = message.channel.id;
+    else {
+      chan = chan[1];
+      response = args.join(" ").replace(CHANNEL_REGEX, "");
+    }
+    if (args.length < 1) throw new this.client.methods.errors.UsageError("You need to give a message to send dummy.", loadingMessage);
+    const channel = message.guild.channels.get(chan);
+    if (channel.permissionsFor(message.member).missing(["SEND_MESSAGES", "VIEW_CHANNEL"]).length) throw new this.client.methods.errors.UsageError("You don't have permission to `say` in that channel dummy.", loadingMessage);
+    if (channel.permissionsFor(message.guild.me).missing(["SEND_MESSAGES", "VIEW_CHANNEL"]).length) throw new this.client.methods.errors.UsageError("I don't have permission to `say` in that channel baka.", loadingMessage);
+    return [channel, response];
+  }
 
-    if (!channel || msg.length < 1) throw new this.client.methods.errors.UsageError("You need to give the bot a message to send.", loadingMessage);
-    msg = msg.join(" ");
-
-    const channelid = await this.verifyChannel(message, channel, { msg: loadingMessage });
-    if (channelid === message.channel.id) msg = channel + " " + msg;    
-    const chn = message.guild.channels.get(channelid);
-    if (chn.permissionsFor(message.guild.me).missing(["SEND_MESSAGES", "READ_MESSAGES"]).length > 0) throw new this.client.methods.errors.UsageError("I do not have permission to `say` in that channel.", loadingMessage);
-    if (chn.permissionsFor(message.member).missing(["SEND_MESSAGES", "READ_MESSAGES"]).length > 0) throw new this.client.methods.errors.UsageError("You do not have permission to `say` in that channel.", loadingMessage);
-    
+  async run(message, args, level, loadingMessage) {
+    const [channel, msg] = await this.cmdVerify(message, args, loadingMessage);
+    const response = !msg ? args.join(" ") : msg;
     message.delete();
 
-    chn.startTyping();
-    setTimeout(() => {
-      chn.send(msg);
-      chn.stopTyping(true);
-    }, 100 * msg.length / 2);
+    channel.startTyping();
+    setTimeout(async () => {
+      switch (message.flags[0]) {
+        case "owo":
+          channel.send(await this.client.idiotAPI.owoify(response));
+          break;
+        case "mock":
+          channel.send(await this.client.idiotAPI.mock(response));
+          break;
+          
+        case "cancer":
+          channel.send(await this.client.idiotAPI.mock(await this.client.idiotAPI.owoify(response)));
+          break;
+
+        default:
+          channel.send(response);
+          break;
+      }
+      channel.stopTyping(true);
+    }, 100 * args.join(" ").length / 2);
   }
 }
 
