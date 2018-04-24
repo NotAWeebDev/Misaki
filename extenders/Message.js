6a48a18909b67c4f473b9a656aa09a1ee50e75ad
AndrewJWin <fastroute66@live.com>
2018-04-24 11:41:58 -0500

    Trivia Update

--- extenders/Message.js
+++ extenders/Message.js
@@ -19,11 +19,11 @@ module.exports = Structures.extend("Message", Message => class extends Message {
     return this.channel.send(`${this.author} \`|${emoji}|\` ${content}`, Object.assign({}, options,  { embed }));
   }
 
-  async awaitReply(question, filter, limit = 60000, embed) {
-    await this.channel.send(question, embed);
-    return this.channel.awaitMessages(filter, { max: 1, time: limit, errors: ["time"] })
+  async awaitReply(question, filter, limit = 60000) {
+    const msg = await this.channel.send(question);
+    return [await this.channel.awaitMessages(filter, { max: 1, time: limit, errors: ["time"] })
       .then(collected => collected.first().content)
-      .catch(() => false);
+      .catch(() => false), msg];
   }
 
 });
